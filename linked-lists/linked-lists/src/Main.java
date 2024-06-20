76befe1f905b05d3c5d746f310b08a668b24c935
koksao <roksanaklodka101@gmail.com>
2024-06-20 20:40:17 +0200

    implementation of singly linked list

--- linked-lists/linked-lists/src/Main.java
+++ linked-lists/linked-lists/src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
 
+        LinkedList list1 = new LinkedList();
+
+        list1.insert(6);
+        list1.insert(7);
+        list1.insert(7);
+        list1.insert(9);
+        list1.insert(12);
+
+        list1.printList();
+
     }
 }
