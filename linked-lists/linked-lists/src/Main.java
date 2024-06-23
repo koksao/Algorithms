07803c70211a7a5e320b68eb9614f4c447c9206b
koksao <roksanaklodka101@gmail.com>
2024-06-23 19:55:16 +0200

    merge two sorted linked-lists

--- linked-lists/linked-lists/src/Main.java
+++ linked-lists/linked-lists/src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         LinkedList list1 = new LinkedList();
+        LinkedList list2 = new LinkedList();
 
         list1.insert(6);
         list1.insert(7);
@@ -9,7 +10,19 @@ public class Main {
         list1.insert(9);
         list1.insert(12);
 
+        list2.insert(3);
+        list2.insert(3);
+        list2.insert(5);
+        list2.insert(15);
+        list2.insert(19);
+
+        list1.printList();
+        System.out.println();
+        list2.printList();
+        System.out.println();
+        list1.mergeLists(list2);
         list1.printList();
 
     }
+
 }
